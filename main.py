b3a1c802146f346dd539c4cb08b9468267c9ef55
kiki-kanri <a470666@gmail.com>
2022-10-07 08:31:25 +0800

    修復不能輸入網址問題

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from wnacg import Wnacg
 
 
 def main():
-    # urls_string = input('請輸入網址或aid，多個請用空格分隔：').strip()
-    urls_string = 'https://wnacg.com/photos-index-aid-121283.html'
+    urls_string = input('請輸入網址或aid，多個請用空格分隔：').strip()
     urls = re.split(r'[\s]+', urls_string)
     wnacg = Wnacg()
     wnacg.start(urls)
